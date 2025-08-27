63af24d95fe09a6f568d1449da3aa956f4ddab08
supuntharindu123 <supuntharindu1125@gmail.com>
2025-08-27 11:04:02 +0530

    feat: initialize frontend and backend with React, Vite,  Tailwind CSS, express, cors, mongoose and dotenv.

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
